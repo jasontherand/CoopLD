22e78590a866b0567532a1750696da3b9a2056ed
Lloyd <lloydaugust@gmail.com>
2017-07-09 14:01:47 -0400

    basic movement script

---
+++ CoopLD/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //player movement 
+    public float speed;
+
+
+    //player's rgidbody
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+        
+    }
+}
