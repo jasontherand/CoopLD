847934cf100a05a01e53a41d33b6a725e8fa96d0
Jason Rand <JasonRand@Dynast11.com>
2017-07-14 16:56:05 -0400

    Created the ship and vertical movement

--- CoopLD/Assets/Scripts/PlayerController.cs
+++ CoopLD/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,20 @@ public class PlayerController : MonoBehaviour {
     //player movement 
     public float speed;
 
+    //Is the player in front of a ladder
+    private bool climbable;
+
 
     //player's rgidbody
     private Rigidbody rb;
+    //player's animator
+    private Animator anim;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,9 +31,63 @@ public class PlayerController : MonoBehaviour {
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        Vector3 movement = rb.velocity;
 
-        rb.AddForce(movement * speed);
-        
+        movement.x = moveHorizontal * speed;
+
+        if (climbable) {
+            float moveVertical = Input.GetAxis("Vertical");
+            movement.y = moveVertical * speed;
+        }
+
+        rb.velocity = movement;
+
+    }
+    private void OnTriggerEnter(Collider theCollider)
+    {
+        Debug.Log("Triggering");
+       if(theCollider.tag == "Ladder")
+        {
+            Debug.Log("Ladder");
+            MakeClimbable();
+        }
+    }
+    private void OnTriggerExit(Collider theCollider)
+    {
+        if (theCollider.tag == "Ladder")
+        {
+            Debug.Log("Ladder");
+            RemoveClimbable();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colliding");
+    }
+
+
+    void MakeClimbable()
+    {
+        //turn off gravity
+        rb.useGravity = false;
+        //allow vertical movement
+        climbable = true;
+        //tell the animator
+        anim.SetBool("Climbable", true);
+    }
+    void RemoveClimbable()
+    {
+        rb.useGravity = true;
+        climbable = false;
+        anim.SetBool("Climbable", false);
+    }
+
+
+
+
+    bool IsClimbable()
+    {
+        return climbable;
     }
 }
